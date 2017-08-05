4eadc3810c083e743ecd5dd273360dd3481b0dc0
Sridhar Ratnakumar <srid@srid.ca>
2017-08-05 00:57:22 +0000

    Update Git ref to point to v0.6.0.0

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ let
   result = import (pkgs.fetchFromGitHub {
     owner = "dmjio";
     repo = "miso";
-    sha256 = "13ckz11gbfs047hl3phj7h6fm59wsg9zw2fiqjaqkxmxv17zj5yj";
-    rev = "0834d5c0b309de24d836cbdcc25fd257de10be17";
+    sha256 = "07v2wrinnvkcmrmrav5xngybbms4scqhrhprbfqyak20snr4ir1q";
+    rev = "e2a3ac05f6b228c6060046fbdbf3d93c4232229c";
   }) {};
 in pkgs.haskell.packages.ghcjs.callPackage ./app.nix {
   miso = result.miso-ghcjs;
